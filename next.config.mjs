52f0bf73a6e571a14bafdc88706f8f6eaa415960
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2024-08-06 21:40:31 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   output: "export",
-  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
