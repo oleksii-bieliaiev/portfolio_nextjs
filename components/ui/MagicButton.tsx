4a2437030134769ba10566dd84345667dbe0b0ba
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2025-03-10 17:45:53 +0100

    app 8

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const MagicButton = ({
   title,
   icon,
@@ -20,10 +18,10 @@ const MagicButton = ({
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-     
+      {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center z-100 rounded-lg
-             bg-[#352F44] px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
@@ -33,4 +31,4 @@ const MagicButton = ({
   );
 };
 
-export default MagicButton
+export default MagicButton;
